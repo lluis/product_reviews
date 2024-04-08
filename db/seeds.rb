e7fc7d1677766b7fd13feca2a0a933fbac753ccc
Lluís Gili <lluisgili@gmail.com>
2024-04-08 20:23:09 +0200

    Add some random products and reviews as seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.destroy_all
+
+# Create 10 random products
+10.times do |i|
+  name = Faker::Food.unique.dish
+  Product.create(
+    name: name,
+    description: Faker::Lorem.paragraph,
+    image_url: Faker::LoremFlickr.image(
+      search_terms: name.split.select {|word| word.length > 3 }
+    ),
+  )
+  # with 0-5 random reviews
+  rand(5).times do
+    Review.create(
+      product: Product.last,
+      text: Faker::Lorem.paragraph,
+      rating: rand(5),
+      author: Faker::Name.name,
+    )
+  end
+end
