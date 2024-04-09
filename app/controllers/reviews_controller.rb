70585901705aecb2b793d5033e09f20ddf470e9a
Lluís Gili <lluis@ingent.net>
2024-04-09 21:58:21 +0200

    Cleanup unused

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,23 +1,9 @@
 class ReviewsController < ApplicationController
   before_action :load_product
-  before_action :set_review, only: %i[ show edit update destroy ]
-
-  # GET /reviews or /reviews.json
-  def index
-    @reviews = @product.reviews
-  end
-
-  # GET /reviews/1 or /reviews/1.json
-  def show
-  end
 
   # GET /reviews/new
   def new
     @review = @product.reviews.build
-  end
-
-  # GET /reviews/1/edit
-  def edit
   end
 
   # POST /reviews or /reviews.json
@@ -30,32 +16,11 @@ class ReviewsController < ApplicationController
         format.html { redirect_to @product, notice: "Review was successfully created." }
         format.json { render :show, status: :created, location: @review }
       else
+        #TODO: Show validation errors with turbo_stream?
+        format.turbo_stream
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @review.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # PATCH/PUT /reviews/1 or /reviews/1.json
-  def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to product_review_url(@product, @review), notice: "Review was successfully updated." }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /reviews/1 or /reviews/1.json
-  def destroy
-    @review.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to product_reviews_url(@product), notice: "Review was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
@@ -63,11 +28,6 @@ class ReviewsController < ApplicationController
 
     def load_product
       @product = Product.find(params[:product_id])
-    end
-
-    # Use callbacks to share common setup or constraints between actions.
-    def set_review
-      @review = @product.reviews.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -6,11 +6,6 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
     @product = @review.product
   end
 
-  test "should get index" do
-    get product_reviews_url(@product)
-    assert_response :success
-  end
-
   test "should get new" do
     get new_product_review_url(@product)
     assert_response :success
@@ -21,29 +16,6 @@ class ReviewsControllerTest < ActionDispatch::IntegrationTest
       post product_reviews_url(@product), params: { review: { author: @review.author, product_id: @review.product_id, rating: @review.rating, text: @review.text } }
     end
 
-    assert_redirected_to product_review_url(@product, Review.last)
-  end
-
-  test "should show review" do
-    get product_review_url(@product, @review)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_product_review_url(@product, @review)
-    assert_response :success
-  end
-
-  test "should update review" do
-    patch product_review_url(@product, @review), params: { review: { author: @review.author, product_id: @review.product_id, rating: @review.rating, text: @review.text } }
-    assert_redirected_to product_review_url(@product, @review)
-  end
-
-  test "should destroy review" do
-    assert_difference("Review.count", -1) do
-      delete product_review_url(@product, @review)
-    end
-
-    assert_redirected_to product_reviews_url(@product)
+    assert_redirected_to product_url(@product)
   end
 end
